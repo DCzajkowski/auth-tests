7f5727cc77f19061b09260568da41a64b8430bc4
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-09-28 17:15:55 +0200

    Fix signature v3

--- src/Console/Commands/AuthTestsMakeCommand.php
+++ src/Console/Commands/AuthTestsMakeCommand.php
@@ -11,8 +11,8 @@ class AuthTestsMakeCommand extends Command
      *
      * @var string
      */
-    protected $name = 'make:auth-tests
-                       {--force : Overwrite existing tests}';
+    protected $signature = 'make:auth-tests
+        {--force : Overwrite existing tests}';
 
     /**
      * The console command description.
